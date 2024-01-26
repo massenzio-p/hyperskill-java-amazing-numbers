0974ab080a47b3669b216a152c26e3247b17f0bf
Max Pestov <pestov.maxik@gmail.com>
2024-01-26 17:19:03 +0400

    First stage completed

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -1,7 +1,13 @@
 package numbers;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-//        write your code here
+        Scanner scanner = new Scanner(System.in);
+        NumbersGame buzzNumberGame = new BuzzNumberGame(scanner);
+        buzzNumberGame.play();
+        scanner.close();
     }
 }
---
+++ Amazing Numbers/task/src/numbers/NumbersGame.java
@@ -0,0 +1,5 @@
+package numbers;
+
+public interface NumbersGame {
+    void play();
+}
